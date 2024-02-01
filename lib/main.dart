0fdb23384c16c57c58f4f8aac7a19017ab495a3e
DannyMarvel <dannymarvey@gmail.com>
2024-02-01 23:09:45 +0100

    fresh

--- lib/main.dart
+++ lib/main.dart
@@ -173,6 +173,9 @@ class _MyHomePageState extends State<MyHomePage> {
                     buildButton('5'),
                     buildButton('6'),
                     buildButton('x'),
+                    buildButton('x'),
+                    buildButton('x'),
+                    buildButton('x'),
                     
                   ],
                 ),
